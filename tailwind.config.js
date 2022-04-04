97f92f37a882b2e6272c9ee386c301490196ccd5
chrisfromhood <kristijan@cogentdeciphering.com>
2022-04-04 20:09:19 +0200

    header

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+
+    backgroundImage: {
+      'hero-picture': "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(../img/hero.jpg)",
+    },
+    colors: {
+      'primary': '#e67e22',
+      'light': '#fdf2e9',
+      'gray': '#555',
+      'dark': '#333'
+    },
+  },
+
+  fontFamily: {
+    rubik: ['Rubik', 'sans-serif'],
+  },
+
+  plugins: [],
+}
