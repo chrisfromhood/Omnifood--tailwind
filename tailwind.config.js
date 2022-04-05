394b215489160238616bd5af5332a8fdf8ea26a3
chrisfromhood <kristijan@cogentdeciphering.com>
2022-04-05 12:19:10 +0200

    inicial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-
     backgroundImage: {
       'hero-picture': "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(../img/hero.jpg)",
     },
     colors: {
       'primary': '#e67e22',
+      'secondary': '#cf711f',
       'light': '#fdf2e9',
       'gray': '#555',
-      'dark': '#333'
+      'dark': '#333',
+      'white': '#fff'
     },
   },
 
