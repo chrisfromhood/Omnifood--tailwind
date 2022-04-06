ed553378795e07c7c1d370d645ae328bafe5ceb7
chrisfromhood <kristijan@cogentdeciphering.com>
2022-04-06 11:57:02 +0200

    working on gallery

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
       'secondary': '#cf711f',
       'light': '#fdf2e9',
       'gray-one': '#888',
+      'gray-two': '#6f6f6f',
       'gray': '#555',
       'dark': '#333',
       'white': '#fff',
@@ -24,6 +25,10 @@ module.exports = {
         'shad': '0 0 0 8px rgba(230, 125, 34,  0.5)',
         'card-shad': '0 24px 48px rgba(0, 0, 0,  0.08)',
       },
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        'fr-55-45': '55fr 45fr',
+      }
     },
 
 
